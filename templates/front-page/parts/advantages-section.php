272f905d4098bed205315eb042e635a8619ebafb
Timofey Krivenkov <drake9100@mail.ru>
2023-10-14 04:17:39 +0300

    UPD: Секция Преимущества

---
+++ templates/front-page/parts/advantages-section.php
@@ -0,0 +1,64 @@
+<div class="advantages">
+  <div class="advantages__container content-container">
+    <h2 class="advantages__title site-title">There are only <span>2 taxes</span> in the UAE and one of the <b>most
+        favorable tax jurisdictions in the world</b></h2>
+    <div class="advantages__layout">
+      <div class="advantages__grid-wrapper">
+        <div class="advantages__grid">
+          <div class="advantages__item advantage">
+            <svg class="advantage__icon" width="100" height="100">
+              <use xlink:href="<?= ct()->get_assets_url() ?>/img/sprite.svg#advantage-1"></use>
+            </svg>
+            <div class="advantage__title">Low taxes</div>
+            <div class="advantage__text">VAT - 5%, corporate tax - 9%. no taxes on capital gains, personal income,
+              salaries and dividends</div>
+          </div>
+          <div class="advantages__item advantage">
+            <svg class="advantage__icon" width="100" height="100">
+              <use xlink:href="<?= ct()->get_assets_url() ?>/img/sprite.svg#advantage-2"></use>
+            </svg>
+            <div class="advantage__title">Stable banking system</div>
+            <div class="advantage__text">Many UAE banks are in the TOP 100 best banks in the world. The UAE banking
+              system is stable – strong capital, sufficient liquidity.</div>
+          </div>
+          <div class="advantages__item advantage">
+            <svg class="advantage__icon" width="100" height="100">
+              <use xlink:href="<?= ct()->get_assets_url() ?>/img/sprite.svg#advantage-3"></use>
+            </svg>
+            <div class="advantage__title">It's easy to get a residence permit</div>
+            <div class="advantage__text">The transfer of family and employees to the UAE becomes available through
+              obtaining a resident visa.</div>
+          </div>
+          <div class="advantages__item advantage">
+            <svg class="advantage__icon" width="100" height="100">
+              <use xlink:href="<?= ct()->get_assets_url() ?>/img/sprite.svg#advantage-4"></use>
+            </svg>
+            <div class="advantage__title">Geographical position</div>
+            <div class="advantage__text">Its geographical location makes the Emirates a key global market with a
+              developed transport network and perfectly streamlined logistics chains.</div>
+          </div>
+        </div>
+        <a class="advantages__button btn--main" href="#callback">Еще преимущества
+          <svg
+            xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 0 29 29" fill="none">
+            <path d="M26.3582 12.7588L1.3584 12.7588L1.3584 15.4762L26.3582 15.4762L26.3582 12.7588Z" fill="white" />
+            <path
+              d="M21.5898 20.8462L19.6684 18.9247L24.4336 14.1594L19.6684 9.39414L21.5898 7.47266L28.2766 14.1594L21.5898 20.8462Z"
+              fill="white" />
+          </svg>
+        </a>
+      </div>
+      <div class="advantages__callback">
+        <div class="advantages__callback-cite">«<span>700+ companies have chosen our expertise</span> to successfully start a business in the UAE. We are located in Dubai and have all the practical experience in the region»</div>
+        <div class="advantages__callback-footer">
+          <img loading="lazy" src="<?= ct()->get_assets_url() ?>/img/advantages-person.png" class="advantages__callback-person" width="125" height="125" alt="">
+          <div class="advantages__callback-info">
+            <div class="advantages__callback-name">Roman Bykov</div>
+            <div class="advantages__callback-role">CCO</div>
+            <button class="btn-reset advantages__callback-button btn--main" data-graph-path="callback-modal">Ask a question</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
