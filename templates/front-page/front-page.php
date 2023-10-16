7b64a99719e88220b9580485af3a4a4c9caa2dd0
Timofey Krivenkov <drake9100@mail.ru>
2023-10-16 03:41:34 +0300

    ADD: Секция с флагами

--- templates/front-page/front-page.php
+++ templates/front-page/front-page.php
@@ -8,5 +8,6 @@
 <?php ct()->template('/front-page/parts/hero-section.php') ?>
 <?php ct()->template('/front-page/parts/services-section.php') ?>
 <?php ct()->template('/front-page/parts/advantages-section.php') ?>
+<?php ct()->template('/front-page/parts/flags-section.php') ?>
 <?php
 ct()->footer();
---
+++ templates/front-page/parts/flags-section.php
@@ -0,0 +1,9 @@
+<section class="flags-section">
+  <div class="content-container flags-section__container">
+    <div class="flags-section__wrapper">
+      <h2 class="flags-section__title">The UAE has not closed the market for Russian entrepreneurs</h2>
+      <div class="flags-section__text">Find out about new opportunities that are not available in Russia during a consultation</div>
+      <button class="btn-reset flags-section__button btn--second" data-graph-path="callback-modal">Get a consultation</button>
+    </div>
+  </div>
+</section>
