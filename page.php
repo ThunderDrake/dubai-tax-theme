4a1b0f2f6bdcccfe9c4e59f2819216bfdfef684b
Timofey Krivenkov <drake9100@mail.ru>
2023-10-21 05:05:37 +0300

    UPD: Дефолтная страница

--- page.php
+++ page.php
@@ -1,7 +1,13 @@
 <?php
 ct()->header()
 ?>
-
+<?php ct()->template('/parts/inner-page-hero.php') ?>
+<section class="page">
+  <div class="page__container content-container">
+    <?php the_content() ?>
+  </div>
+</section>
+<?php ct()->template('/front-page/parts/preferences-section.php') ?>
 <?php
 ct()->footer()
 ?>
