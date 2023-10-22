275fc16d2ee3e8a3df116d66d4acf9374bef6096
Timofey Krivenkov <drake9100@mail.ru>
2023-10-23 01:34:01 +0300

    ADD: Секция с Квизом

--- templates/front-page/front-page.php
+++ templates/front-page/front-page.php
@@ -7,6 +7,7 @@
 ?>
 <?php ct()->template('/front-page/parts/hero-section.php') ?>
 <?php ct()->template('/front-page/parts/services-section.php') ?>
+<?php ct()->template('/front-page/parts/quiz-section.php') ?>
 <?php ct()->template('/front-page/parts/advantages-section.php') ?>
 <?php ct()->template('/front-page/parts/flags-section.php') ?>
 <?php ct()->template('/front-page/parts/steps-section.php') ?>
