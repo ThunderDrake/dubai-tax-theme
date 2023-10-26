61951d9e86e1f99165d82b6ea3727f54755022b1
Timofey Krivenkov <drake9100@mail.ru>
2023-10-27 02:47:44 +0300

    UPD: Секция квиза

--- templates/front-page/parts/quiz-section.php
+++ templates/front-page/parts/quiz-section.php
@@ -1,5 +1,5 @@
 <section class="quiz" id="quiz">
   <div class="quiz__container content-container">
-  <iframe id="marquiz__inline-frame_650d95232a0e530025b8e0ac" class="marquiz__inline-frame" src="//quiz.marquiz.ru/650d95232a0e530025b8e0ac?href=https%3A%2F%2Ftaxtva.com%2F&amp;mode=inline&amp;referrer=https%3A%2F%2Ftaxtva.com%2Fopen_company_free" scrolling="auto" frameborder="0" allow="autoplay" style="width: 100%; height: auto; aspect-ratio: 16/9;"></iframe>
+  <iframe id="marquiz__inline-frame_650d95232a0e530025b8e0ac" class="marquiz__inline-frame" src="//quiz.marquiz.ru/650d95232a0e530025b8e0ac?href=https%3A%2F%2Ftaxtva.com%2F&amp;mode=inline&amp;referrer=https%3A%2F%2Ftaxtva.com%2Fopen_company_free" scrolling="auto" frameborder="0" allow="autoplay" style="width: 100%; height: 100%; aspect-ratio: 16/9;"></iframe>
   </div>
 </section>
